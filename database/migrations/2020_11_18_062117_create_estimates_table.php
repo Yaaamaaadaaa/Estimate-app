aa42ba2159058a8aa4607f9ab3283aa88cb062f8
山田翔太 <yamadashouta@yamadashoutanoMacBook-Air.local>
2020-11-18 15:58:38 +0900

    [add]estimatesテーブルとitemsテーブルの作成

---
+++ database/migrations/2020_11_18_062117_create_estimates_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEstimatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estimates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('item_id')->unsigned();
+            $table->string('title', 100);
+            $table->string('location', 100);
+            $table->string('transaction', 100);
+            $table->string('effectiveness', 100);
+            $table->string('customer', 100);
+            $table->date('deadline_at');
+            $table->date('estimated_at');
+            $table->timestamps();
+
+            $table->foreign('item_id')->references('id')->on('items');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('estimates');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/estimates/', 'EstimateController@index')->name('estimates.index');
