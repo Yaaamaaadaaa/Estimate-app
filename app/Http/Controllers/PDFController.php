23add3d3ba0eef54e2df1f2ab7c40848db43d83a
山田翔太 <yamadashouta@yamadashoutanoMacBook-Air.local>
2020-12-13 16:19:46 +0900

    [add]基本的なpdf作成機能を追加

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -2,14 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Estimate;
+use App\Item;
 use Illuminate\Http\Request;
 use PDF;
 
 class PDFController extends Controller
 {
-    public function index(){
+    public function index(Request $request){
+        $estimate_id = $request->input('estimate');
+        $estimate = Estimate::find($estimate_id);
+        $items = Item::where('estimate_id', $estimate_id)->get();
 
-    	$pdf = PDF::loadView('pdf/generate_pdf');
+    	$pdf = PDF::loadView('pdf/generate_pdf', [
+            'estimate' => $estimate,
+            'items' => $items,
+        ]);
 
     	return $pdf->stream();
 
--- resources/views/pdf/generate_pdf.blade.php
+++ resources/views/pdf/generate_pdf.blade.php
@@ -21,6 +21,40 @@
     </style>
 </head>
 <body>
-日本語の表示
+  <h1 style="text-align:center;">御見積書</h1>
+    <table border="1" width="100%" cellspacing="0" cellpadding="0"　style="table-layout: fixed">
+      <tr>
+        <th style="width: 100px">商品名</th>
+        <th style="width: 15px">単位</th>
+        <th style="width: 10px">数量</th>
+        <th style="width: 25px">単価</th>
+        <th style="width: 25px">金額</th>
+        <th style="width: 25px">備考</th>
+      </tr>
+      @foreach ($items as $item)
+        <tr>
+          <td style="width: 100px">
+            {{ $item['name'] }}
+          </td>
+          <td style="width: 15px">
+            {{ $item['unit'] }}
+          </td>
+          <td style="width: 10px">
+            {{ $item['quantity'] }}
+          </td>
+          <td style="width: 25px">
+            {{ $item['unit_price'] }}
+          </td>
+          <td style="width: 25px">
+            {{ $item['quantity'] * $item['unit_price'] }}
+          </td>
+          <td style="width: 25px">
+            {{ $item['other'] }}
+          </td>
+        </tr>
+      @endforeach
+    </table>
+  <script src="{{ asset('js/generate_pdf.js')}}"></script>
+  <script src="https://cdn.jsdelivr.net/npm/vue@2.6.11"></script>
 </body>
 </html>
