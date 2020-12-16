9384a616f2c8e870ebe4b5066eb675c41ba1e642
山田翔太 <yamadashouta@yamadashoutanoMacBook-Air.local>
2020-12-16 13:53:09 +0900

    [add]pdfのレイアウトを作成

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -13,9 +13,17 @@ class PDFController extends Controller
         $estimate_id = $request->input('estimate');
         $estimate = Estimate::find($estimate_id);
         $items = Item::where('estimate_id', $estimate_id)->get();
+        $item_price = Item::where('estimate_id', $estimate_id)->get(['quantity', 'unit_price']);
+        $sum_price = 0;
+
+        for($i=0; $i<count($item_price); $i++){
+            $calculation_price = $item_price[$i]['quantity'] * $item_price[$i]['unit_price'];
+            $sum_price += $calculation_price;
+        }
 
     	$pdf = PDF::loadView('pdf/generate_pdf', [
             'estimate' => $estimate,
+            'sum_price' => $sum_price,
             'items' => $items,
         ]);
 
--- resources/views/pdf/generate_pdf.blade.php
+++ resources/views/pdf/generate_pdf.blade.php
@@ -22,37 +22,94 @@
 </head>
 <body>
   <h1 style="text-align:center;">御見積書</h1>
-    <table border="1" width="100%" cellspacing="0" cellpadding="0"　style="table-layout: fixed">
+  <h2>{{ $estimate['customer'] }}様</h2>
+  <p>下記の通り御見積申し上げます</p>
+  <p>件名：{{ $estimate['title'] }}</p>
+  <p>納入期限：{{ $estimate['deadline_at'] }}</p>
+  <p>納入場所：{{ $estimate['location'] }}</p>
+  <p>取引方法：{{ $estimate['transaction'] }}</p>
+  <p>有効期限：{{ $estimate['effectiveness'] }}</p>
+  <h2>御見積合計金額　¥{{ number_format($sum_price * 1.1) }}-</h2>
+    <table border="1" width="100%" cellspacing="0" cellpadding="0"　style="table-layout: auto">
       <tr>
-        <th style="width: 100px">商品名</th>
-        <th style="width: 15px">単位</th>
-        <th style="width: 10px">数量</th>
-        <th style="width: 25px">単価</th>
-        <th style="width: 25px">金額</th>
-        <th style="width: 25px">備考</th>
+        <th>商品名</th>
+        <th>単位</th>
+        <th>数量</th>
+        <th>単価</th>
+        <th>金額</th>
+        <th>備考</th>
       </tr>
       @foreach ($items as $item)
         <tr>
-          <td style="width: 100px">
+          <td>
             {{ $item['name'] }}
           </td>
-          <td style="width: 15px">
+          <td style="text-align:center">
             {{ $item['unit'] }}
           </td>
-          <td style="width: 10px">
+          <td style="text-align:right">
             {{ $item['quantity'] }}
           </td>
-          <td style="width: 25px">
-            {{ $item['unit_price'] }}
+          <td style="text-align:right">
+            {{ number_format($item['unit_price']) }}
           </td>
-          <td style="width: 25px">
-            {{ $item['quantity'] * $item['unit_price'] }}
+          <td style="text-align:right">
+            {{ number_format($item['quantity'] * $item['unit_price']) }}
           </td>
-          <td style="width: 25px">
+          <td style="font-size:12px">
             {{ $item['other'] }}
           </td>
         </tr>
       @endforeach
+        <tr>
+          <td style="text-align:right">
+            ＜税抜合計金額＞
+          </td>
+          <td>
+          </td>
+          <td>
+          </td>
+          <td>
+          </td>
+          <td style="text-align:right">
+            {{ number_format($sum_price) }}
+          </td>
+          <td>
+          </td>
+        </tr>
+        <tr>
+          <td style="text-align:right">
+            ＜消費税＞
+          </td>
+          <td>
+          </td>
+          <td>
+          </td>
+          <td>
+          </td>
+          <td style="text-align:right">
+            {{ number_format($sum_price * 0.1) }}
+          </td>
+          <td>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            毎度ありがとうございます。
+          </td>
+          <td>
+          </td>
+          <td>
+          </td>
+          <td style="text-align:center">
+          合計
+          </td>
+          <td style="text-align:right">
+            {{ number_format($sum_price * 1.1) }}
+          </td>
+          <td>
+          </td>
+        </tr>
     </table>
   <script src="{{ asset('js/generate_pdf.js')}}"></script>
   <script src="https://cdn.jsdelivr.net/npm/vue@2.6.11"></script>
